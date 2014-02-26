8ef612b8cefe77dc216486d9113734048f81f2f5
Tyson Pitt <tysonpitt@gmail.com>
2014-02-25 20:15:42 -0500

    Add partials and AJAX

--- app/controllers/line_items_controller.rb
+++ app/controllers/line_items_controller.rb
@@ -31,7 +31,8 @@ class LineItemsController < ApplicationController
 
     respond_to do |format|
       if @line_item.save
-        format.html { redirect_to @line_item.cart }
+        format.html { redirect_to store_url }
+        format.js { @current_item = @line_item }
         format.json { render action: 'show',
           status: :created, location: @line_item }
       else
@@ -61,7 +62,7 @@ class LineItemsController < ApplicationController
   def destroy
     @line_item.destroy
     respond_to do |format|
-      format.html { redirect_to @line_item.cart, notice: 'Item was successfully removed.' }
+      format.html { redirect_to line_items_url }
       format.json { head :no_content }
     end
   end
